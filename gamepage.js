f78a8be3c1825556d65bdf32201eac1cdd3e90c7
Gandham Dheeraj <121343901+Technoblade19876@users.noreply.github.com>
2023-05-31 16:31:50 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,37 @@
+// generation of random value
+var y = Math.floor(Math.random() * 10 - 1);
+console.log(y);
+
+// count of attempts
+guess = 1;
+// until hit
+
+playername = localStorage.getItem("player_name");
+
+// function for the number sent by the user
+
+
+console.log(x);
+function submit() {
+  var x = document.getElementById("Guess_text").value;
+
+    if (x == y) {
+        alert(
+          "CONGRATULATIONS!!!" +
+            playername +
+            "YOU GUESSED IT RIGHT IN" +
+            guess +
+            " GUESS"
+        );
+      } else if (x < y) {
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+      } else {
+        guess++;
+        alert("OOPS SORRY!! TRY A GREATER NUMBER");
+      }
+}
+
+function playAgain() {
+  y = Math.floor(Math.random() * 10 - 1);
+}
